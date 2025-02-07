7ae351fe1feb0df337d8e41ebcde8d5662d311ae
sangwoo <hun1246@hanmail.net>
2025-02-07 17:41:15 +0900

    주석추가

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -9,5 +9,6 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
     }
-
+//        주석추가
+//     어떻게 커밋하지??
 }
